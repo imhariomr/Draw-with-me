20ad6e71ae965539778330ffc2358a7fdd441ffc
imhariomr <imhariomr@gmail.com>
2025-10-21 23:49:02 +0530

    added port

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 import room from './routes/room.route'
 const app = express();
 app.use(express.json());
-const port = process.env.HTTP_PORT;
+const port = process.env.HTTP_PORT || 4000;
 app.use(cors());
 
 app.use('/api/v1',room);
